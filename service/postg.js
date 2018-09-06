7794b76f1881764c1b132f8825d9df420a342063
Christopher Devers <cmdaese@gmail.com>
2018-09-06 03:14:01 +0000

    Cleaned up.  Documented.

--- service/postg.js
+++ service/postg.js
@@ -1,9 +1,21 @@
-var config=require('config');
-const { Pool } =  require('pg')
+/*
+A helper object for persisting tournment data to our postgres instance.
 
+C Devers 5 SEP 2018
+*/
+
+var config=require('config');  //we use this library for standard configuration.
+const { Pool } =  require('pg') //This is the postgress connection library we use.
+
+/*
+This object contains everything we need to persist tournament data.  It has
+constants that define common SQL for use against the database, methods for
+connection handling, and then methods for the tournament service Express app to 
+consume are defined and made available through export.
+*/
 var gresHelper = (function(){
-    // declare private variables and/or functions.
 
+    //SQL to get a list of tournments.
     const tournament = "SELECT tournament_id, location_name, location_address, sanction_request_date, sanction_approval_date, " +
        "tournamnet_start_date," +
        "registration_time," +
@@ -12,7 +24,8 @@ var gresHelper = (function(){
        "rain_date_registration_time," +
        "rain_date_event_1_start_time" +
        "FROM public.tournament"
-       
+
+    //SQL to insert a tournment and get an ID back.       
     const insertTournament = "insert into tournament(" +
         "location_name," +
         "location_address," +
@@ -30,7 +43,8 @@ var gresHelper = (function(){
         "values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)" +
         "RETURNING tournament_id"
 
-const updateTournament = "update public.tournament set " +
+    //SQL for updating an existing tournment given its ID.
+    const updateTournament = "update public.tournament set " +
         "location_name = $1," +
         "location_address = $2," +
         "location_city = $3," +
@@ -45,7 +59,10 @@ const updateTournament = "update public.tournament set " +
         "rain_date_event_1_start_time = $12" +
         "where tournament_id = $13;"
 
-
+    /*
+    Declare the connection pool that will manage subsequent connections used
+    by this helper
+    */
     const pool = new Pool({
             user: config.Database.user,
             host: config.Database.host,
@@ -54,23 +71,14 @@ const updateTournament = "update public.tournament set " +
             port: config.Database.port,
         })
     
-    var poolConn = function(){
-
-    
-        console.log('Trying to connect...');
-    
-        pool.query('SELECT event_id,name FROM public.event', (err, res) => {
-            console.log(err, res)
-            pool.end()
-        })
-    }
-
+    //Get a list of tournaments.
     var getTournaments = async function(){
 
         return await pool.query("SELECT tournament_id, location_name FROM public.tournament");
 
     }
     
+    //Get a single tournament given its ID.
     var getTournament = async function(id){
                 return await pool.query("SELECT tournament_id,"+
                                        "location_name,"+
@@ -87,6 +95,7 @@ const updateTournament = "update public.tournament set " +
                                        "rain_date_event_1_start_time FROM public.tournament where tournament_id = $1", [id]);
     }
     
+    //Insert a new tournament.
     var createTournament = async function(input){
         var now = new Date();
         var values = [input.location_name, input.location_address, input.location_city, input.location_state, now, now, input.tournament_start_date, input.registration_start_time, input.event_1_start_time, input.rain_date, input.rain_date_registration_time, input.rain_date_event_1_start_time]
@@ -95,69 +104,37 @@ const updateTournament = "update public.tournament set " +
         
     }
     
+    //Change an existing tournament given its udpated data and its ID.
     var changeTournament = async function(input){
         var now = new Date();
-        /*COA1: Change the method to be synchronous.  
-            Pro:  Fixes async/promise issue.
-            Con:  Synchronous calls are a performance hit.
-            
-          COA2: Return a generic promise.
-            Pro: Allows ansyc operations.
-            Con: I don't know how to do it (yet).
-        */
 
+        /*
+        Because we're using updating a particular row, we want the transaction
+        to be safe from interference from another call to update another
+        tournament.  For this, we createa  client from the pool to immediately
+        discarded after the transaction is complete.
+        */
         const client = await pool.connect()
-//                            .then(()=> console.log('connected'))
-//                            .catch(e => console.error('connection error', e.stack))
         var values = [input.location_name, input.location_address, input.location_city, input.location_state, now, now, input.tournament_start_date, input.registration_start_time, input.event_1_start_time, input.rain_date, input.rain_date_registration_time, input.rain_date_event_1_start_time, input.tournament_id]
-        //client.query('SET AUTOCOMMIT TO ON')
+
         client.query(updateTournament, values)
           .then(result => console.log(result))
           .catch(e => console.error(e.stack))
           .then(() => client.end())
           .catch(e => console.error("error ending client " + e.stack))
-          
-          
-          
-/*        client.query('COMMIT', (err) => {
-          if (err) {
-            console.error('Error committing transaction', err.stack)
-          }
-        })
-*/         
-        //client.release();
+
+        client.release();
         
-/*        
-        try {
-            var values = [input.location_city, input.location_state, input.tournament_id]
-            client.query('BEGIN')
-            .then(() => console.log("BEGIN"))
-            .catch(() => console.log("BEGIN FAIL"))
-            
-            client.query(updateTournament, values)
-            .then(() => console.log("UPDATE"))
-            .catch(() => console.log("UPDATE FAIL"))
-            
-            client.query('COMMIT;')
-            .then(() => console.log("COMMIT"))
-            .catch(() => console.log("COMMIT FAIL"))
-            console.log("updated for " + input.tournament_id)
-        }catch(e){
-            client.query('ROLLBACK')
-            console.log("Rolling back! " + e.message)
-            throw e
-        }finally{
-            client.release()
-        }
-*/
+        /*
+        Since this call is asynchronous, we must return a promise. Here, we
+        have a little hack to satisfy this condition.  Tech debt.
+        */
         return new Promise(()=> console.log("I'm a Trump promise!")); 
     }
     
     return{
         
     // declare public variables and/or functions.
-    
-    poolConn: poolConn,
     
     getTournaments: getTournaments,
     
